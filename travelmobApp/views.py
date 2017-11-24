6f7960ef287b2894aa28c2825a6017212ec2c815
Ahmed Elemam <eMaM151987@gmail.com>
2017-11-24 14:32:44 +0200

    Update views.py

--- travelmobApp/views.py
+++ travelmobApp/views.py
@@ -79,7 +79,7 @@ def scrap(request):
 
         return HttpResponse(json.dumps(ret, ensure_ascii=False))
     else:
-        cities = City.objects.values('name').filter(name__startswith='e').distinct()
+        cities = City.objects.values('name').filter(name__startswith='f').distinct()
         flipKey = FlipKeyScrapper()
         for city in cities:
             print 'start processing with city ' + str(city['name'])
